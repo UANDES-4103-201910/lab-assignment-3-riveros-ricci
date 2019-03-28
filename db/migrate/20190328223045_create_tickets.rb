7e5fdae4e29aead580e216e44efb44da1f81ad77
Pedro <priccis@gmail.com>
2019-03-28 19:42:57 -0300

    Fixed decimal type in ticket's price and order's total_price

--- db/migrate/20190328223045_create_tickets.rb
+++ db/migrate/20190328223045_create_tickets.rb
@@ -3,8 +3,7 @@ class CreateTickets < ActiveRecord::Migration[5.2]
     create_table :tickets do |t|
       t.references :event, foreign_key: true
       t.references :category, foreign_key: true
-      t.decimal10 :price
-      t.decimal2 :price
+      t.decimal :price
       t.references :order, foreign_key: true
 
       t.timestamps
